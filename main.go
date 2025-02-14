db444e025126d0ecf864cdd9164226b702a1db39
Harshit <108566178+Developer-Harshit@users.noreply.github.com>
2025-02-14 15:56:34 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +15,7 @@ import (
 var page string
 
 func main() {
+	http.HandleFunc("/test", test)
 	http.HandleFunc("/", serveHello)
 
 	port := os.Getenv("PORT")
@@ -26,6 +28,15 @@ func main() {
 	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal("Error starting server:", err)
 	}
+}
+func test(rw http.ResponseWriter, req *http.Request) {
+    body, err := io.ReadAll(req.Body)
+    if err != nil {
+        panic(err)
+    }
+    log.Println(string(body))
+    fmt.Fprint(rw,"hi")
+
 }
 
 func serveHello(w http.ResponseWriter, r *http.Request) {
